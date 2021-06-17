afa706fde1db4243a0e436a91fda2d54870bcd64
Laure <nilugette@yahoo.fr>
2021-06-17 10:07:19 +0200

    Passwords all checks done!

--- src/pages/Password.js
+++ src/pages/Password.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { patchAccount } from "../redux/account/account.action";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
 
 
 const required = (value) => {
@@ -21,20 +23,19 @@ const vpassword = (value) => {
         </div>
       );
     }
-  };
-
-const vconfirmedPassword = (value) => {
-    if (value.length < 8 || value.length > 40) {
-        return (
-        <div className="alert alert-danger" role="alert">
-            The confirmed password must be between 8 and 40 characters.
-        </div>
-        );
-    }
 };
 
-const Password = () => {
+const vdontMatch = () => {
+  return (
+    <div className="alert alert-danger" role="alert">
+      it's doesn't match
+    </div>
+  );    
+};
 
+
+const Password = () => {
+  const form = useRef();
   //const account = useSelector(state => state.accountReducer)
   const [password, setPassword] = useState("")
   const [passwordConfirmed, setPasswordConfirmed] = useState("")
@@ -43,23 +44,23 @@ const Password = () => {
 
   const handlePatchAccountPassword = async (e) => {
     e.preventDefault() 
-    if(password === passwordConfirmed) {
-        const patchAccountPasswordData = {
-            password: password,
-        }
-        dispatch(patchAccount(patchAccountPasswordData))
-    } else {
-        console.log("password and confirmed password doesn't match")
-    }
-  }
 
+    if(password === passwordConfirmed) {
+      const patchAccountPasswordData = {
+        password: password,
+      } 
+      dispatch(patchAccount(patchAccountPasswordData))
+     } else {
+       vdontMatch()
+      }
+  }
   
   return (
     <div className="container">
-        <form  onSubmit={e => handlePatchAccountPassword(e)}>
+        <Form  onSubmit={e => handlePatchAccountPassword(e)} ref={form}>
             <div className="form-group text-center">
                 <label htmlFor="newPassword">Indiquez votre nouveau mot de passe</label>
-                <input 
+                <Input 
                     type="password" 
                     className="form-control" 
                     id="newPassword" 
@@ -70,20 +71,21 @@ const Password = () => {
             </div>
             <div className="form-group text-center">
                 <label htmlFor="confirmedPassword">Confirmer votre nouveau mot de passe</label>
-                <input 
+                <Input 
                     type="password" 
                     className="form-control" 
                     id="confirmedPassword" 
                     name="confirmed_password" 
                     value={passwordConfirmed}
                     onChange={ e => setPasswordConfirmed(e.target.value)}
-                    validations={[required, vconfirmedPassword]} />
+                    validations={[required, vpassword, vdontMatch]} />
+           
             </div>
 
             <div className="text-center">
                 <button type="submit" className="btn btn-warning">Je valide</button>
             </div>   
-        </form>
+        </Form>
     </div>
   );
 };
