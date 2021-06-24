a0ab0bc35aae03005dd184d81ac2a3e4b4abb5af
Laure <nilugette@yahoo.fr>
2021-06-24 14:59:02 +0200

    Added condition when currentUser exist don't display create an account in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,26 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import { Link } from "react-router-dom"
 
 
 const Footer = () => {
+    const { user: currentUser } = useSelector((state) => state.auth);
     return (
         <div>
             <footer className="footer">
                 <div className="container">
                     <nav className="navbar navbar-expand-md justify-content-center navbar-dark fixed-bottom bg-dark">
                         <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link className="nav-link" to="/auth/inscription">Créer mon compte</Link>
-                            </li>
+                            {
+                                 !currentUser? (
+                                     <>
+                                        <li className="nav-item">
+                                            <Link className="nav-link" to="/auth/inscription">Créer mon compte</Link>
+                                        </li>
+                                     </>
+                                 ): ("") 
+                            }
+                            
                             <li className="nav-item">
                                 <Link className="nav-link" to="#">Afficher par catégorie</Link>
                             </li>
